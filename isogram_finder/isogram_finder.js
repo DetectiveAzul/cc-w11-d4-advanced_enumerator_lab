e948e398cab6c005888494d3f6505546c6933582
Jaime Lopez <detec.azul@gmail.com>
2018-05-17 11:00:08 +0100

    Isogram done and tested.

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,19 @@
 const IsogramFinder = function (word) {
-
+  this.word = this.toObj(word.toLowerCase());
 }
 
 IsogramFinder.prototype.isIsogram = function () {
+  return !Object.keys(this.word).some( (letter) => {
+    return this.word[letter] > 1;
+  })
+}
 
+IsogramFinder.prototype.toObj = function(word) {
+  let wordObj = {};
+  word.split('').forEach( (letter) => {
+    (wordObj[letter]) ? wordObj[letter] ++ : wordObj[letter] = 1;
+  });
+  return wordObj;
 }
 
 module.exports = IsogramFinder;
