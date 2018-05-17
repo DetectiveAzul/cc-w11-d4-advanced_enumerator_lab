43e38de9a6cd2e2e45943117cf3144148398498a
Jaime Lopez <detec.azul@gmail.com>
2018-05-17 11:17:55 +0100

    Anagram done and tested

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,20 @@
 const AnagramFinder = function (word) {
-
+this.word = word;
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+  return otherWords.filter( (word) => {
+    return this.isAnagram(word);
+  });
+}
 
+AnagramFinder.prototype.isAnagram = function (word) {
+  const sameSize = (this.word.length === word.length);
+  const sameLetters = this.word.split('').every( (letter) => {
+    return word.toLowerCase().includes(letter.toLowerCase());
+  });
+  const notSameWord = this.word !== word;
+  return sameSize && sameLetters && notSameWord;
 }
 
 module.exports = AnagramFinder;
